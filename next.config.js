023f2cd497d7bfd9f8e82916aee465d2e79795e2
digitalarchivo <161184382+digitalarchivo@users.noreply.github.com>
2025-04-23 08:04:47 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Required for GitHub Pages and static deployments
-  // For GitHub Pages, you'll need this if deploying to a custom domain
-  // basePath: '/your-repo-name',
+  reactStrictMode: true,
   images: {
     unoptimized: true, // Required for static export
-  },
+  }
 }
 
 module.exports = nextConfig
